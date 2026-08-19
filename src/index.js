a0a17222a07d76bfee6718643f98a44ef48c7e8d
SnowyRawrGamer <joel@freshideawebsites.com>
2026-08-19 09:34:49 -0400

    Remove outdated Node entrypoint after Vite migration

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// Entry point for the snowyparty project.
-
-export function main() {
-  return "snowyparty is ready!";
-}
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  console.log(main());
-}
